1ed84fee238dc1ea58472a1f31323a0f80b0b859
michaels90up <michael1990spw@gmail.com>
2022-09-08 20:45:56 +0200

    Update libraries

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// import cheerio from 'cheerio';
+import cheerio from 'cheerio';
 import fetch from 'node-fetch';
+
+const allImagesArray = [];
 
 fetch('https://memegen-link-examples-upleveled.netlify.app/')
   .then((response) => response.text())
