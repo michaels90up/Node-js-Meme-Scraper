5fccfe2eb4a23d80ac5c5e429bdf2faea4c02a62
michaels90up <michael1990spw@gmail.com>
2022-09-08 21:36:33 +0200

    Changing libraries

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import cheerio from 'cheerio';
+// import * as cheerio from 'cheerio';
+// import fs from 'fs';
+// import got from 'got';
 import fetch from 'node-fetch';
 
-const allImagesArray = [];
+const myUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
+fetch(myUrl)
   .then((response) => response.text())
   .then((data) => console.log(data))
   .catch(() => {});
