3e02fea94f8ce7736ff9ef5b5624adeaf8e2407b
michaels90up <michael1990spw@gmail.com>
2022-09-09 14:32:17 +0200

    Adding array

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-// import * as cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
-// // import got from 'got';
-// fetch(myUrl)
+// this one works:
+// fetch('https://memegen-link-examples-upleveled.netlify.app/')
 //   .then((response) => response.text())
 //   .then((data) => console.log(data))
 //   .catch(() => {});
 
-async function needMemes() {
-  const requestUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
-  const request = new Request(requestUrl);
-  const response = await fetch(request);
-  const storeString = await response.json();
-}
-console.log(needMemes());
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const fullText = await response.text();
+const $ = cheerio.load(fullText);
+let div = $('section > div');
+
+let fullArray = [];
+div.each(function () {
+  fullArray.push($(this).find('img').attr('src'));
+});
