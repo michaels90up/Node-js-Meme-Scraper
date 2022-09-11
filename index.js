3a3124f938324e3e02bd5feb1470ecc47481f814
michaels90up <michael1990spw@gmail.com>
2022-09-11 20:58:01 +0200

    Update comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import fetch from 'node-fetch';
 
 // let counter = 1;
-
+// Alternative solution path:
 // fetch('https://memegen-link-examples-upleveled.netlify.app/')
 //   .then((response) => response.text())
 //   .then((data) => console.log(data))
