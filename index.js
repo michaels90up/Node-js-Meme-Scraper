30f48b7dab762572aad9ea2958043bcd0d6e9d21
michaels90up <michael1990spw@gmail.com>
2022-09-11 21:13:04 +0200

    Update comments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import fetch from 'node-fetch';
 
 // let counter = 1;
+
 // Alternative solution path:
 // fetch('https://memegen-link-examples-upleveled.netlify.app/')
 //   .then((response) => response.text())
@@ -25,17 +26,5 @@ div.each(function () {
 const newArray = fullArray.splice(0, 10);
 console.log(newArray);
 
-// const getMemes = await fetch(newArray);
-//
-// function saveImgToFolder(url, path) {
-//   const localPath = fs.createWriteStream(path);
-//
-//   https.get(url, function (response) {
-//     response.pipe(localPath);
-//     console.log('success');
-//   });
-// }
-// firstTenMemes.forEach((meme) => {
-//   saveImgToFolder(meme, `./memes/0${counter}.jpg`);
-//   counter++;
-// });
+// fs.writefile
+// fs.write
